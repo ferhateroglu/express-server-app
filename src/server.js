9a678e9357e9e832c3a22ee250aa9388dd55c0ad
Ferhat Eroglu <ferhateroglu1907@gmail.com>
2021-12-12 00:28:26 +0300

    partials registered

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const app = express();//Expresi kullanmak için gereken başlangıç fonksiyonu
 const path = require('path');
-const PUBLIC_PATH_URL = path.join(__dirname,'../public');//index.html in konumu
-app.set('view engine', 'hbs');
-app.use(express.static(PUBLIC_PATH_URL));//statik içerik servis etme
+const hbs = require('hbs');
+
+const PUBLIC_PATH_URL = path.join(__dirname,'../public');
+const TEMPLATES_URL = path.join(__dirname,'../templates');
+const PARTİAL_URL = path.join(__dirname,'../templates/partials');
+
+
+const app = express();//Expresi kullanmak için gereken başlangıç fonksiyonu
+app.set('view engine', 'hbs');//views engine ve views i güncellemek
+app.set('views', TEMPLATES_URL);//default olarak hbs views dosyasyı altında arama yapar biz bunun yerine templates klasörü oluşturduk
+
+
+app.use(express.static(PUBLIC_PATH_URL));//public klasörünü servis etme
+hbs.registerPartials(PARTİAL_URL);//partial klasörünü kayıt etme
 
 //app.get() fonksiyonu string türünde bir PATH alır.
 //PATH domainden sonraki kısımdır ör: ferhateroglu.com/blog 
