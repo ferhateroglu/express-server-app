6aaae0ecf42c25fa4e18f2e688b685ec46fba35e
Ferhat Eroglu <ferhateroglu1907@gmail.com>
2021-12-12 20:39:44 +0300

    add click event, fetch

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,32 @@
-console.log('hi');
+const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-input');
+const p1 = document.querySelector('#p1');
+const p2 = document.querySelector('#p2');
+const p3 = document.querySelector('#p3');
+
+searchForm.addEventListener('submit', (event) => {
+
+    const il = searchInput.value;
+    const FETCH_URL = 'http://localhost:3000/forecast?adress='+il;
+
+    event.preventDefault();//default olarak submit butonu sayfayı yeniler bunu kapatmak için bu fonksiyon kullanılır
+    
+    //get isteği atma
+    //.then'den sonrası get requestten gelen cevap
+    fetch(FETCH_URL).then((response) => {
+        response.json().then((data) => {
+            console.log(data);
+            if(typeof(data.error) !== 'undefined'){
+                alert(data.error);
+            }
+            else{
+                p1.innerText = 'İl: '+ data.adres;
+                p2.innerText = 'Sıcaklık: '+ data.sıcaklık;
+                p3.innerText = 'Rüzgar: '+ data.rüzgar;
+            }
+        });
+    });
+});
+
+
+
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const request = require('request');
 
 const PUBLIC_PATH_URL = path.join(__dirname,'../public');
 const TEMPLATES_URL = path.join(__dirname,'../templates');
@@ -19,8 +20,7 @@ hbs.registerPartials(PARTİAL_URL);//partial klasörünü kayıt etme
 //PATH domainden sonraki kısımdır ör: ferhateroglu.com/blog 
 app.get('/',(request, response) => {
     response.render('index',{
-        title: 'Ana Sayfa',
-        body: 'ara metin'
+        title: 'Ana Sayfa'
     });
 });
 app.get('/about',(request, response) => {
@@ -35,11 +35,36 @@ app.get('/help',(request, response) => {
 });
 
 app.get('/forecast',(req,res) => {
-    console.log(req.query);
-    res.send({
-        weather:'Güneşli',
-        adress: req.query.adress
-    });
+
+    const il = req.query.adress;//forecast?adress=mersin şeklinde yapılan sorguyu alır
+    const URL = 'http://api.weatherapi.com/v1/current.json?key=2fab9888c0ee43d3a53190750211012&q='+il+'&aqi=no';
+
+
+    if(!il){
+        res.send({
+            error:'adres bilgisini girmediniz'
+        });
+    }
+    else{
+        //({url:''},(response,error) =>{}); şeklinde parametre alır.
+        request({url: URL,json:true }, (error,response) => {
+            if(typeof(response.body.error) !== 'undefined'){
+                res.send({
+                    error: 'bölge bilgisi bulunamadı'
+                });
+            }
+            else{
+                const {temp_c,wind_kph}=response.body.current;
+                //res.send(il+' ilinde hava: '+temp_c+' derece \n'+'Rüzgar: '+ wind_kph+'km/s');
+                res.send({
+                    adres: il,
+                    sıcaklık:temp_c.toString(),
+                    rüzgar:wind_kph.toString()
+                });
+            }       
+        });
+    }
+
 });
 
 // * tüm get isteklerini kapsar öcneki get isteklerine girmediyse buraya girer
